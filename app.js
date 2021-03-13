66a376bfa376beca9129c4a0c963c04c0f2fcc06
Christianbenjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2021-03-13 10:27:25 +0100

    Build app with Vue

---
+++ app.js
@@ -0,0 +1,28 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = ''
+    }
+  }
+}).mount('#app');
+
+// const buttonEL = document.querySelector('button');
+// const inputEL = document.querySelector('input');
+// const listEL = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEL.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEL.appendChild(listItemEl);
+//   inputEL.value = '';
+// }
+
+// buttonEL.addEventListener('click', addGoal)
